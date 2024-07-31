a8fe919e14b45990ceacbab5ffa966bc2e11cd2b
CharlyCst <charly.castes@epfl.ch>
2024-07-31 20:19:23 +0200

    "Update website (automated)"

---
+++ assets/js/814f3328.3be643f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite_miralis=self.webpackChunkwebsite_miralis||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hello, world!","permalink":"/blog/hello-world","unlisted":false}]}')}}]);
